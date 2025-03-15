b94cb7a3253afb4911d276c7f7e67b197b3579fb
Welton <welton.garcia@sistematic.net.br>
2025-03-15 16:54:18 -0400

    Initial commit

---
+++ layouts/footer.php
@@ -0,0 +1,47 @@
+<footer class="mt-5 py-5">
+    <div class="container bg-dark">
+        <div class="row g-3">
+            <div class="col-md-3 text-center p-3 rounded shadow">
+                <img src="assets/imgs/logo.png" width="100px" />
+                <p>Os melhores produtos pelo melhor preço.</p>
+            </div>
+            <div class="col-md-3 p-3 rounded shadow">
+                <h3 class="text-center">Principais Páginas</h3>
+                <ul class="list-unstyled text-start">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 p-3 rounded shadow">
+                <h3 class="text-center">Contato</h3>
+                <p><strong>Telefone:</strong> (11) 1234-5678</p>
+                <p><strong>Email:</strong> contato@loja.com</p>
+                <p><strong>Endereço:</strong> Rua Exemplo, 123 - Centro, São Paulo - SP</p>
+            </div>
+            <div class="col-md-3 bg-white p-3 rounded shadow text-center">
+                <h3>Produtos em Destaque</h3>
+                <div class="d-flex flex-wrap justify-content-center gap-2">
+                    <img src="https://picsum.photos/60" alt="Produto 1" class="rounded shadow">
+                    <img src="https://picsum.photos/61" alt="Produto 2" class="rounded shadow">
+                    <img src="https://picsum.photos/62" alt="Produto 3" class="rounded shadow">
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col text-center text-white p-3 rounded">
+                    <p class="mb-1">eCommerce @ 2022 All Right Reserved</p>
+                    <div>
+                        <a href="#" class="text-white"><i class="fab fa-facebook"></i></a> |
+                        <a href="#" class="text-white"><i class="fab fa-instagram"></i></a> |
+                        <a href="#" class="text-white"><i class="fab fa-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+        <!-- Logo à esquerda -->
+        <a class="navbar-brand" href="#">
+            <img src="assets/imgs/logo.png" width="100px" />
+        </a>
+
+        <!-- Botão do menu responsivo -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Menu alinhado à direita -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Produtos</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Fale Conosco</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-user"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
