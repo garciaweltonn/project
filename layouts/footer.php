6cf8e14c4109a2492e3bf33c67b524a739a1132b
Welton <welton.garcia@sistematic.net.br>
2025-03-15 17:42:38 -0400

    projeto finalizado

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,31 +1,33 @@
-<footer class="mt-5 py-5">
-    <div class="container bg-dark">
+<footer class="mt-3 py-5">
+    <div class="bg-dark" id="footer">
         <div class="row g-3">
             <div class="col-md-3 text-center p-3 rounded shadow">
-                <img src="assets/imgs/logo.png" width="100px" />
-                <p>Os melhores produtos pelo melhor preço.</p>
+                <img src="assets/imgs/logo.png" width="150px" />
+                <p class="text-white">Os melhores produtos pelo melhor preço.</p>
             </div>
-            <div class="col-md-3 p-3 rounded shadow">
-                <h3 class="text-center">Principais Páginas</h3>
+            <div class="col-md-3 p-3 rounded shadow text-white">
+                <h4 class="text-center">Principais Páginas</h4>
                 <ul class="list-unstyled text-start">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Serviços</a></li>
-                    <li><a href="#">Contato</a></li>
+                    <li><a class="text-white" href="#">Home</a></li>
+                    <li><a class="text-white" href="#">Sobre</a></li>
+                    <li><a class="text-white" href="#">Serviços</a></li>
+                    <li><a class="text-white" href="#">Contato</a></li>
                 </ul>
             </div>
-            <div class="col-md-3 p-3 rounded shadow">
-                <h3 class="text-center">Contato</h3>
+            <div class="col-md-3 p-3 rounded shadow text-white">
+                <h4 class="text-center">Contato</h4>
                 <p><strong>Telefone:</strong> (11) 1234-5678</p>
                 <p><strong>Email:</strong> contato@loja.com</p>
                 <p><strong>Endereço:</strong> Rua Exemplo, 123 - Centro, São Paulo - SP</p>
             </div>
-            <div class="col-md-3 bg-white p-3 rounded shadow text-center">
-                <h3>Produtos em Destaque</h3>
+            <div class="col-md-3 p-3 rounded shadow text-center">
+                <h4>instagram</h4>
                 <div class="d-flex flex-wrap justify-content-center gap-2">
                     <img src="https://picsum.photos/60" alt="Produto 1" class="rounded shadow">
                     <img src="https://picsum.photos/61" alt="Produto 2" class="rounded shadow">
                     <img src="https://picsum.photos/62" alt="Produto 3" class="rounded shadow">
+                    <img src="https://picsum.photos/63" alt="Produto 4" class="rounded shadow">
+                    <img src="https://picsum.photos/64" alt="Produto 5" class="rounded shadow">
                 </div>
             </div>
             <div class="row mt-4">
